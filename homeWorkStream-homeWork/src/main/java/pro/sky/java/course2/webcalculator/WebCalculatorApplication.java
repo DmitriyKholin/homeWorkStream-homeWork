fcc4b07fe18319d72138b57c8ceec2b59505bb05
Dmitriy <bcezbs@gmail.com>
2022-10-03 18:52:00 +0300

    homework

--- homeWorkStream-homeWork/src/main/java/pro/sky/java/course2/webcalculator/WebCalculatorApplication.java
+++ homeWorkStream-homeWork/src/main/java/pro/sky/java/course2/webcalculator/WebCalculatorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebCalculatorApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(WebCalculatorApplication.class, args);
     }
